e619fcbe7be108c10f36cc6e0597119aeeec5923
unknown <your@mom.com>
2020-05-22 14:43:50 -0700

    please work

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -9,21 +9,50 @@ export class SignupComponent implements OnInit {
 
   constructor() { }
   
-  password : String = "asdfa";
-  cPassword : String = "asdfads";
+  password : String = "";
+  cPassword : String = "";
+  disable = false;
+
+  validPassword = true;
+  validEmail = true;
+
 
   ngOnInit() {
+
     
   }
-  passwordValidate(event) {
-    console.log(this.password);
-    console.log(this.cPassword);
+
+
+  ngAfterViewInit(){
+    console.log("test");
+    this.emailValid();
+    this.passwordValidate();
+  }
+  passwordValidate() {
+    let inputs = document.getElementsByTagName("input");
+    let passwords = [inputs[2], inputs[3]];
     if(this.password === this.cPassword){ 
-      event.target.setCustomValidity('');
+      (passwords[0] as HTMLInputElement).setCustomValidity('');
+      (passwords[1] as HTMLInputElement).setCustomValidity('');
+      this.validPassword = false;
     } else {
       console.log("should reach here");
-      event.target.setCustomValidity('Password Must be Matching.');
+      passwords[0].setCustomValidity('Password Must be Matching.');
+      passwords[1].setCustomValidity('Password Must be Matching.');
+      this.validPassword = true;
     }
-    
+    this.updateButton();
+  }
+
+  emailValid(){
+    let email = <HTMLInputElement>(document.getElementById("email"))
+    this.validEmail = !email.validity.valid;
+    console.log("password is valid?: " + email.validity.valid);
+    this.updateButton();
+  }
+
+  updateButton(){
+    this.disable = this.validPassword === true || this.validEmail === true;
+    console.log(this.disable);
   }
 }
